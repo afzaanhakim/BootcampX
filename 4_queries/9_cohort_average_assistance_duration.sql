bcde54906e2389e9b675d56a4f727fa9d4aa5f77
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-22 19:49:00 +0000

    linted this file

--- 4_queries/9_cohort_average_assistance_duration.sql
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -1,4 +1,5 @@
-SELECT AVG(total_duration) AS average_total_duration FROM ( 
+SELECT AVG(total_duration) AS average_total_duration 
+FROM ( 
 SELECT cohorts.name AS cohort, SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
 FROM assistance_requests
 JOIN students on students.id = assistance_requests.student_id
