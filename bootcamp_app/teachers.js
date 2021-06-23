02a13a53bdee8fece5c6bad6032bcea2e10ce8e6
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-23 23:10:09 +0000

    create teachers.js and used queries from queries_4 file, created a connection using code from students.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,41 @@
+const {Pool} = require('pg');
+
+const pool = new Pool ({
+user: 'vagrant',
+password: '123',
+host: 'localhost',
+database: 'bootcampx',
+port: 5432
+});
+
+
+
+pool.connect().then(() => {
+  console.log('connected');
+}).catch (e => {
+console.log("errorrrrrrrr");
+console.log(e);
+
+});
+
+
+const args = process.argv.slice(2);
+
+pool.query(`
+SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE ('${args[0]}%')
+ORDER BY cohorts.name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch((error) => {
+  console.log('ERRORRRRRRRRR');
+  console.log(error);
+});
