b2f44df5f5653e68e833a9755d9bd69e8e932d9f
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-24 00:09:53 +0000

    parameterized queries to secure from SQL Injection attacks

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -17,18 +17,21 @@ pool
     console.log(e);
   });
 
-const args = process.argv.slice(2);
-
-pool
-  .query(
-    `
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE ('${args[0]}%')
-LIMIT ${args[1]};
-`
-  )
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
